dd5ee5ccc402b44a840cad9203c5016b3ebacdca
TruongThinh14 <95459900+TruongThinh14@users.noreply.github.com>
2023-12-26 20:27:36 +0700

    host updated

--- Host.h
+++ Host.h
@@ -1,46 +1,5 @@
 #include <iostream>
-#include <vector>
+#include "Host.h"
 
-class Review {
-    // Review class implementation
-};
-
-class Host {
-private:
-    std::vector<int> hostRatingScore;
-    std::vector<Review*> allHostReviews;
-
-public:
-    double getAverageHostRating() {
-        double average = 0.0;
-        if (!hostRatingScore.empty()) {
-            int sum = 0;
-            for (int rating : hostRatingScore) {
-                sum += rating;
-            }
-            average = static_cast<double>(sum) / hostRatingScore.size();
-        }
-        return average;
-    }
-
-    bool bookAvailability() {
-        // Check for availability logic
-        return true; // Placeholder return value
-    }
-
-    void showAllAvailableSupporters(const std::string& fromTime, const std::string& toTime) {
-        // Show available supporters logic
-    }
-};
-
-int main() {
-    // Test the Host class
-    Host host;
-
-    // Call the methods
-    double averageRating = host.getAverageHostRating();
-    bool isAvailable = host.bookAvailability();
-    host.showAllAvailableSupporters("9:00 AM", "5:00 PM");
-
-    return 0;
-}
+using std::string;
+using std::cout;
